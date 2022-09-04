99be2097516932a3504ce0878a9e4c4db07f5c49
mrted <mailmrted@gmail.com>
2022-09-05 00:46:07 +0100

    set ALLOWED_HOSTS

--- multi_step_form/settings.py
+++ multi_step_form/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rg6qx=x-!pw09ch*+xv1)pn5adh_%=(07id*7ysq*b+eh(51$=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
