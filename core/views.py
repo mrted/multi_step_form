481ba876cc978d3d221c9f4a0fb881c47a5ec8f1
mrted <mailmrted@gmail.com>
2022-09-04 23:02:20 +0100

    created customer create view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from core.models import Customer
 
-# Create your views here.
+
+def create_customer(request):
+    if request.method == 'POST' and 'profile_pic' in request.FILES:
+        first_name = request.POST.get('firstname')
+        last_name = request.POST['last_name']
+        profile_pic = request.FILES['profile_pic']
+        try:
+            customer = Customer.objects.create(
+                first_name=first_name ,
+                last_name=last_name ,
+                profile_pic=profile_pic, 
+                )
+            customer.save()
+            messages.success(request, 'Customer created successfully')
+        except:
+            messages.error(request, 'Customer creation failed')
+    return render(request, 'create-customer.html')
+
