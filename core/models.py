b64ce2bd3dce59402ccd9a9a3127c5499693a84e
mrted <mailmrted@gmail.com>
2022-09-05 00:03:40 +0100

    updates

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
+    middle_name = models.CharField(max_length=20, null=True)
     profile_pic = models.ImageField(upload_to='images/profile_pictures')
 
     def __str__(self):
--- core/views.py
+++ core/views.py
@@ -5,13 +5,15 @@ from core.models import Customer
 
 def create_customer(request):
     if request.method == 'POST' and 'profile_pic' in request.FILES:
-        first_name = request.POST.get('firstname')
-        last_name = request.POST['last_name']
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        middle_name = request.POST.get('middle_name')
         profile_pic = request.FILES['profile_pic']
         try:
             customer = Customer.objects.create(
-                first_name=first_name ,
-                last_name=last_name ,
+                first_name=first_name,
+                last_name=last_name,
+                middle_name=middle_name,
                 profile_pic=profile_pic, 
                 )
             customer.save()
